a26381662a8bd16593fd3b2548324025eb5816af
jmzhang1911 <jmzhang1911@gmail.com>
2025-12-17 11:53:16 +0800

    add

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 # sc-pp-0.1.sif
 # apptainer build  ./sifs/sc-pp-0.1.sif ./defs/sc-pp-0.1.def
 # sc-pp-0.2.sif
-# apptainer build  ./sifs/sc-pp-0.2.sif ./defs/sc-pp-0.2.def
+apptainer build  ./sifs/sc-pp-0.2.sif ./defs/sc-pp-0.2.def
 
 ####### sc-basic
 # Seurat scvi-env Signac 
@@ -37,7 +37,7 @@
 
 ####### pySCENIC
 # apptainer build sifs/pyscenic-0.12.1.sif docker://172.16.3.223:5000/aertslab/pyscenic:0.12.1
-apptainer build ./sifs/scenic-0.1.sif ./defs/scenic-0.1.def
+# apptainer build ./sifs/scenic-0.1.sif ./defs/scenic-0.1.def
 ####### vep
 # apptainer build sifs/ensembl-vep-latest.sif docker://hub.jmzhang.click/ensemblorg/ensembl-vep:latest
 
